b06fa8f08af46a2db9bc0d8eed9c43284cdf340b
Raffaele Gatta <163126175+raffytorres96@users.noreply.github.com>
2026-05-14 18:46:56 +0200

    feat: add user data model

---
+++ src/github_analyzer/models.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Optional
+from datetime import datetime
+
+@dataclass
+class GitHubUser:
+    login: str
+    public_repos: int
+    followers: int
+    html_url: str
+    created_at: datetime
+    name: Optional[str] = None
+    city: Optional[str] = None
+    bio: Optional[str] = None
+    hireable: Optional[bool] = None
+    email: Optional[str] = None
